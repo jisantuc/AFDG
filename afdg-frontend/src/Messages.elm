af307c0bff05856ef0f2fd303e4e85ac4910dada
James Santucci <james.santucci@gmail.com>
2017-12-29 14:02:40 -0500

    Project reorganization (#12)
    * Add frontend project setup
    
    * Move backend project setup
    
    * Make docker-compose prettier
    
    * Factor out messages into a module and update infra
    
    * Remove built js file

---
+++ afdg-frontend/src/Messages.elm
@@ -0,0 +1,15 @@
+module Messages exposing (Msg(..))
+
+{-| This module contains all of the message types that can be used in updates to the
+application state
+
+
+# Exported
+
+@doc Msg, Reset
+
+-}
+
+
+type Msg
+    = Reset
